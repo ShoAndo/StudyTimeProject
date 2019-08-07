4fb6b6e16b0439f1261c1649c0835469c2a92741
Sho Ando <spdtas0223@gmail.com>
2019-08-07 09:30:28 +0800

    リリースしたいリリースしたいリリースしたリリースさせろ

---
+++ StudyTimeProject/TimeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimeViewController.swift
+//  StudyTimeProject
+//
+//  Created by 安藤奨 on 2019/08/07.
+//  Copyright © 2019 安藤奨. All rights reserved.
+//
+
+import UIKit
+
+class TimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
